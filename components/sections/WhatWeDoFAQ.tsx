80d3260c45329f91d043bb2b1c9771ac0f181eac
jasna-art <jasna@bbp-india.com>
2026-05-02 17:05:55 +0530

    Update WhatWeDoFAQ.tsx

--- components/sections/WhatWeDoFAQ.tsx
+++ components/sections/WhatWeDoFAQ.tsx
@@ -19,7 +19,6 @@
  * If you edit the visible questions, edit the schemaData too.
  * ============================================================
  */
-import { WhatWeDoFAQ } from "@/components/sections/WhatWeDoFAQ";
 import Link from 'next/link';
 
 // ============================================================
