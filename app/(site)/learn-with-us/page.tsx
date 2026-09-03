1f9b87dcb56d7a952d6a79e1eb815c5166d4ce4a
jasna-art <jasna@bbp-india.com>
2026-09-03 15:24:52 +0530

    Update page.tsx

--- app/(site)/learn-with-us/page.tsx
+++ app/(site)/learn-with-us/page.tsx
@@ -61,7 +61,7 @@ export default function LearnWithUsPage() {
           </dl>
         </div>
         <div className="mt-12">
-          <TextCTA href="mailto:hello@bbp-india.com?subject=Brand%20OS%E2%84%A2%20Practitioner%20Program">
+          <TextCTA href="mailto:hello@bbp-india.com">
             Request the program brief
           </TextCTA>
         </div>
